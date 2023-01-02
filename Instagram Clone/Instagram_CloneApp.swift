b2e1756594f901e0a3822ee4cb56d2a76b1a41a6
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2023-01-02 22:42:42 +0530

    Initial Commit

---
+++ Instagram Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram Clone
+//
+//  Created by Dalveer singh on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
